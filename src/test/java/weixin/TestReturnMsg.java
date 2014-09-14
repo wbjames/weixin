b4efd3f46a445648462e6ac758306d77e73e2522
wbjames <314771755@qq.com>
2014-09-14 17:32:12 +0800

    已实习翻译功能

--- src/test/java/weixin/TestReturnMsg.java
+++ src/test/java/weixin/TestReturnMsg.java
@@ -3,6 +3,7 @@ package weixin;
 import junit.framework.TestCase;
 
 import com.wb.business.basedata.AppBean;
+import com.wb.business.translate.LangTranslate;
 import com.wb.business.wxMsgHandler.ReturnMsg;
 
 /**
@@ -13,8 +14,15 @@ import com.wb.business.wxMsgHandler.ReturnMsg;
  */
 public class TestReturnMsg extends TestCase {
 	
-	public void testSentTextMsg(){
-		AppBean._accessToken = "pVqfOWPXf7dUtztcVXW3hHtcT8lyR52iqE_EB40FBIs1942P_halxYI9U99SOWmHr8NuQbwakbX8wXbw-RaXFA";
-		ReturnMsg.sendTextMsg("o3nDIjlamLDcpeRIXxAYZ8_3fwBg", "text", "中文:dd");
+//	public void testSentTextMsg(){
+//		AppBean._accessToken = "pVqfOWPXf7dUtztcVXW3hHtcT8lyR52iqE_EB40FBIs1942P_halxYI9U99SOWmHr8NuQbwakbX8wXbw-RaXFA";
+//		ReturnMsg.sendTextMsg("o3nDIjlamLDcpeRIXxAYZ8_3fwBg", "text", "中文:dd");
+//	}
+	
+	public void testTranslate(){
+		LangTranslate lt = new LangTranslate();
+		lt.translate("                  \n你好");
+		AppBean._accessToken = "JtUUMCABy-zw3DMTtV7-GzN0QGwgj5SIeH-2rDJJ2jTLrk_2Se4GcjBROgrDXuLp0QMV5ptgnZqz6no8r1fh_Q";
+		ReturnMsg.sendTextMsg("o3nDIjlamLDcpeRIXxAYZ8_3fwBg", "text", AppBean.MENU);
 	}
 }
